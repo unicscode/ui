75f9f2bb646d602d10e364bd01df1878f3018e03
unicscode <lounas.koceila@gmail.com>
2024-02-08 16:52:54 +0100

    add storybook accessibility

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -3,6 +3,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { Button } from "./Button";
 
 const meta: Meta<typeof Button> = {
+  title: "Design System/Atoms/Button",
   component: Button,
 };
 
